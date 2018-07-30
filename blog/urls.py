e07592381e70eaa10a1bbfd9290d20ce98d0705b
Mathilde <mathilde.ziboura@abc-culture.fr>
2018-07-30 10:41:54 +0200

    add test_produit page

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^dp/B000E5RKOA', views.produit, name='produit'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,16 @@ from .forms import PostForm
 
 # Create your views here.
 
+
 def post_list(request):
     posts  = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,5 +43,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def product(request):
+    return render(request, 'blog/test_produit.html')
+
 
 
